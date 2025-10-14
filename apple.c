eb079f02aff20ad359f4986f2e416e5cc43ff0a1
Amez-cky <3473866286@qq.com>
2025-10-14 23:47:55 +0800

    Create apple.c

---
+++ apple.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int m, t, s, n;
+    scanf("%d %d %d", &m, &t, &s);
+    if (t == 0)
+    {
+        printf("0");
+    }
+    else
+    {
+        if (s % t == 0)
+        {
+            n = s / t;
+        }
+        else
+        {
+            n = (s / t) + 1;
+        }
+        if (m <= n)
+        {
+            printf("0");
+        }
+        else
+        {
+            printf("%d", m - n);
+        }
+    }
+
+    return 0;
+}
